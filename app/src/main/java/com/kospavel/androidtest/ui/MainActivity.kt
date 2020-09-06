1c4e6b1757980d4e40dc33269adc094cb99758f0
Pavel Kos <kos.pav007@yandex.ru>
2020-09-06 15:55:20 +0300

    navigation rdy

--- app/src/main/java/com/kospavel/androidtest/ui/MainActivity.kt
+++ app/src/main/java/com/kospavel/androidtest/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.kospavel.androidtest.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.kospavel.androidtest.R
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/com/kospavel/androidtest/ui/mainfeed/MainFeedFragment.kt
@@ -0,0 +1,12 @@
+package com.kospavel.androidtest.ui.mainfeed
+
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.kospavel.androidtest.R
+
+class MainFeedFragment : Fragment(R.layout.fragment_main_feed) {
+
+    init {
+        Log.i("qwerty", "fragment rdy")
+    }
+}
